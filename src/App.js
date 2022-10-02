aa8906431e9cc896822dbcd9723373d6790c07d5
Eric <ettrudell17@gmail.com>
2022-10-01 21:00:41 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h2 >Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Snowboard in Alaska</li>
+        <li>Run an ultra-marathon</li>
+        <li>Pet all the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
